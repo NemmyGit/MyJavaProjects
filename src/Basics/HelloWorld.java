9990e114de9b3fcb4ee97611decaba904b958756
NemmyGit <71345624+NemmyGit@users.noreply.github.com>
2020-09-15 15:54:24 -0400

    Update HelloWorld.java

--- src/Basics/HelloWorld.java
+++ src/Basics/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// Create Hello world
 		System.out.println("Hello Mr. Nehemiah");
-		System.out.println("GitHub Clone update Test");
+		System.out.println("GitHub push Clone update Test");
+		System.out.println("Pull from GitHub");
 
 	}
 
